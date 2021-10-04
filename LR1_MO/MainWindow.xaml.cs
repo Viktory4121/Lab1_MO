8dfc78b3083512973f1e6fd7f9f786ad87e7cafd
Виктория Медведева <ima.aksaki@gmail.com>
2021-10-04 20:27:00 +0300

    Добавьте файлы проекта.

---
+++ LR1_MO/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LR1_MO
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            TextBox textBox1 = new TextBox();
+            TextBox textBox2 = new TextBox();
+            TextBox textBox3 = new TextBox();
+            TextBox textBox4 = new TextBox();
+
+            int m = Convert.ToInt32(textBox1.Text);
+            int n = Convert.ToInt32(textBox2.Text);
+
+            double[,] A = new double[m, n];               //Матрица А
+            double[,] B = new double[m, 1];               //Матрица b
+
+            string s = textBox3.Text;
+            char[] s2 = { ' ', '\n', ',', ';' };
+            string[] s1 = s.Split(s2);
+
+
+
+        }
+    }
+}
